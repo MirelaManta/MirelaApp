20b07bd30b0130d18f498a81ed3b9ba9ba8570e0
MirelaManta <mirela.manta81@yahoo.com>
2023-01-19 11:41:29 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,16 @@
+Mirela's Secret Notes App
+
+Summary: I've created a small application where you can sign up, login and store whatever "secret notes" you want to. They are going to be password protected, so well-kept. Just give it a try.
+
+Setup and installation:\
+-> make sure you have installed the latest Python version\
+-> git clone <repo-url>\
+-> pip install requirements.txt
+
+To run the app:\
+-> run main.py
+
+To view the app:\
+-> go to http://127.0.0.1:5000
+
+
---
+++ website/views.py
@@ -0,0 +1,36 @@
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from .models import Note
+from . import db
+import json
+
+views = Blueprint('views', __name__)  # views as a blueprint means that is has a bunch of routes inside it.
+
+
+# creating the routes(the URLs where users can actually go to)
+@views.route('/', methods=['GET', 'POST'])
+@ login_required  # cannot go to the home page unless you're logged in
+def home():
+    if request.method == 'POST':
+        note = request.form.get('note')
+
+        if len(note) < 5:
+            flash('Note is too short! Give it another try, words are meaningful...\U0001F609', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note added and well kept! \U0001F92B', category='success')
+    return render_template("home.html", user=current_user)
+
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+    return jsonify({})
